3ab3545b1915b3c6caaf227ad94425b86d1fd9ac
Kateriina <blondinkova@mail.ru>
2021-03-07 19:25:19 +0300

    change text and add click on image

--- app/src/main/java/com/example/lesson1/MainActivity.kt
+++ app/src/main/java/com/example/lesson1/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.lesson1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mHelloTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mHelloTextView = findViewById(R.id.textView)
+        var imageButton: ImageButton = findViewById(R.id.imageButton)
+
+        imageButton.setOnClickListener {
+            mHelloTextView.setText("Hello Friend!")
+            mHelloTextView.setTextColor(Color.DKGRAY);
+        }
     }
 }
